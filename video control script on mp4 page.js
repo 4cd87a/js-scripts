5cf0741bfdfb9cf0eee2fc35078957df7013cd79
4cd87a <52211599+4cd87a@users.noreply.github.com>
2021-01-06 20:47:11 +0100

    added support of zoom.us site

--- video control script on mp4 page.js
+++ video control script on mp4 page.js
@@ -1,21 +1,48 @@
 // ==UserScript==
-// @name         Video control
+// @name         Video control on mp4 pages
 // @version      0.1
 // @description  Control speed with numbers and jump over video with arrows
 // @author       4cd87a
 // @match        http://*/*.mp4
 // @match        https://*/*.mp4
+// @match        https://*.zoom.us/*
+// @match        https://filesicfp.phys.ens.fr/*
+
+
 // @grant        none
 // ==/UserScript==
 
 console.log('Video control script');
 
+var currentvideo = 0;
+
+Object.defineProperty(HTMLMediaElement.prototype, 'playing', {
+    get: function(){
+        return !!(this.currentTime > 0 && !this.paused && !this.ended && this.readyState > 2);
+    }
+})
+
 function keyfunc(e) {
-	// console.log(e.keyCode)
+	//console.log(e.keyCode)
 	if (e.keyCode==37) {
-		document.getElementsByTagName("video")[0].currentTime -=10};
+        console.log("step +10s");
+		document.getElementsByTagName("video")[currentvideo].currentTime -=10};
 	if (e.keyCode==39) {
-		document.getElementsByTagName("video")[0].currentTime +=10};
+        console.log("step -10s");
+		document.getElementsByTagName("video")[currentvideo].currentTime +=10};
+
+    if (e.keyCode==32) {
+		if (document.getElementsByTagName("video")[currentvideo].playing) {
+            console.log("pause");
+            document.getElementsByTagName("video")[currentvideo].pause();
+        } else {
+            console.log("play");
+            document.getElementsByTagName("video")[currentvideo].play();
+        };
+        e.stopPropagation();
+        e.preventDefault();
+    }
+
 	if (e.keyCode>=48 && e.keyCode<=57) {
 
 		// 9->2.5
@@ -38,6 +65,21 @@ function keyfunc(e) {
 		}
 
 	}
+	if (e.keyCode==70) {
+        console.log("Enter/exit fullscreen");
+		var fullscreenstate = document.fullScreen || document.mozFullScreen || document.webkitIsFullScreen;
+		fschange(document.getElementsByTagName("video")[currentvideo], (fullscreenstate+1)%2)
+	}
 };
+
+
+function fschange(element, i) {
+	if (i==0) {
+		document.exitFullscreen()
+	} else {
+		element.requestFullscreen()
+	}
+
+}
 
 document.addEventListener("keydown", keyfunc);
